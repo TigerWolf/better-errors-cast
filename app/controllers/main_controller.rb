0ab4b0b6439d73617ed7d4f05119f6b2b8dddf6c
Kieran <kieran@ubuntu.(none)>
2013-04-01 21:16:50 +1030

    Initial rails setup and installed with a sample controller and an error

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,5 @@
+class MainController < ApplicationController
+  def index
+    nil.empty?
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 BetterErrorsCast::Application.routes.draw do
+  get "main/index"
+  root :to => "main#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +51,7 @@ BetterErrorsCast::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+ #  root :to => 'index'
 
   # See how all your routes lay out with "rake routes"
 
